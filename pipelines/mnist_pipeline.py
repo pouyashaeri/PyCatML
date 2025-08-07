f3d6fe6f421493c349ecd3a2b7a5ca5cc0341c78
Pouya <57077710+pouyashaeri@users.noreply.github.com>
2025-08-06 23:49:17 -0700

    added the pipelines

--- pipelines/mnist_pipeline.py
+++ pipelines/mnist_pipeline.py
@@ -1,1 +1,34 @@
 # MNIST categorical pipeline
+import torch
+from torchvision import transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+from ..categories.image import ImageCategory
+from ..functors.base import Functor
+
+class MNISTPipeline:
+    def __init__(self):
+        self.image_category = ImageCategory()
+        self.transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        self.pipeline_functor = Functor(
+            obj_map=self.transform,
+            morph_map=lambda f: lambda x: self.transform(f(x))
+        )
+
+    def load_sample(self):
+        dataset = MNIST(root='./data', train=True, download=True, transform=None)
+        img, label = dataset[0]
+        tensor_img = transforms.ToTensor()(img)
+        self.image_category.add_image_object(tensor_img, {"label": label, "source": "raw"})
+        return tensor_img, label
+
+    def run(self):
+        raw_img, label = self.load_sample()
+        processed_img = self.pipeline_functor.apply_obj(raw_img)
+        self.image_category.add_image_object(processed_img, {"label": label, "stage": "processed"})
+        return processed_img, label
--- pipelines/multimodal_pipeline.py
+++ pipelines/multimodal_pipeline.py
@@ -1,1 +1,34 @@
 # Multimodal learning pipeline
+import torch
+from torchvision import transforms
+from ..categories.image import ImageCategory
+from ..categories.text import TextCategory
+from ..categories.multimodal import ProductCategory
+from ..functors.fusion import fusion_functor
+
+class MultimodalPipeline:
+    def __init__(self, vision_encoder, text_encoder, fusion_module):
+        self.image_category = ImageCategory()
+        self.text_category = TextCategory()
+        self.multi_category = ProductCategory((self.image_category, self.text_category))
+
+        self.vision_encoder = vision_encoder
+        self.text_encoder = text_encoder
+        self.fusion_module = fusion_module
+
+        self.fusion_functor = fusion_functor(vision_encoder, text_encoder, fusion_module)
+
+    def load_sample(self):
+        img = torch.randn(1, 28, 28)
+        text = "A handwritten digit image"
+
+        self.image_category.add_image_object(img, {"source": "synthetic"})
+        self.text_category.add_text_object(text, {"lang": "en"})
+
+        self.multi_category.add_multimodal_object((img, text))
+        return (img, text)
+
+    def run(self):
+        img, text = self.load_sample()
+        fused = self.fusion_functor.apply_obj((img, text))
+        return fused
